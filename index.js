1a4c42ae7deeb86b12e20ae0360ae17a672d1310
ASobrinoB <telemensajes@hotmail.com>
2024-06-10 18:52:56 -0400

    3rd commit

--- index.js
+++ index.js
@@ -12,54 +12,34 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api', userRoutes)
 
 // Definiendo las habitaciones
-
-let totalCapacity = 0;
-let capacity = [];
+const capacity = [];
 
 capacity.push = new Room ("Miramar-101", 2, 2);
-totalCapacity++;
 capacity.push = new Room ("Miramar-102", 4, 1);
-totalCapacity++;
 capacity.push = new Room ("Miramar-103", 6, 2);
-totalCapacity++;
 capacity.push = new Room ("Miramar-104", 2, 1);
-totalCapacity++;
 capacity.push = new Room ("Miramar-105", 4, 2);
-totalCapacity++;
 capacity.push = new Room ("Miramar-106", 6, 1);
-totalCapacity++;
 capacity.push = new Room ("Miramar-107", 2, 2);
-totalCapacity++;
 capacity.push = new Room ("Miramar-108", 4, 1);
-totalCapacity++;
 capacity.push = new Room ("Miramar-109", 6, 2);
-totalCapacity++;
 capacity.push = new Room ("Miramar-110", 2, 1);
-totalCapacity++;
 
 capacity.push = new Room ("Enjoy-101", 2, 2);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-102", 4, 4);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-103", 6, 2);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-104", 2, 4);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-105", 4, 2);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-106", 6, 4);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-107", 2, 2);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-108", 4, 4);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-109", 6, 2);
-totalCapacity++;
 capacity.push = new Room ("Enjoy-110", 2, 4);
-totalCapacity++;
 
-console.log (capacity);
-
+for (let i = 0; i < capacity.length; i++){
+    console.log(capacity[i]);
+}
+  
 app.listen(port, () => {
     console.log('Servidor iniciado en el puerto ' + port);
 })
